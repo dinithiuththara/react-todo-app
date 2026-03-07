61eb8addca2081df04abcc1799d6d5f99f0eab11
dinithiuththara <uththaradinithi20@gmail.com>
2026-03-07 16:16:34 +0530

    Setup Tailwind CSS in React project and verify installation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Text
+    <div className="min-h-screen bg-blue-500 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        My React + Tailwind App
+      </h1>
+
+      <button className="mt-6 px-4 py-2 bg-white text-blue-500 rounded-lg shadow">
+        Click Me
+      </button>
     </div>
   );
 }
